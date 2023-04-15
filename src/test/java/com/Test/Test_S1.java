a54a18980b7665c2d2f198ddceec8a2df917b585
GovindPatil75 <mulegr75@gmail.com>
2023-04-15 18:47:08 +0530

    test_s1

---
+++ src/test/java/com/Test/Test_S1.java
@@ -0,0 +1,14 @@
+package com.Test;
+
+import org.testng.annotations.Test;
+
+import com.Utility.BaseClass;
+
+public class Test_S1 extends BaseClass{
+	
+	@Test
+	public void test_tc001() {
+		System.out.println("Test 1");
+	}
+
+}
